7d61f4a6c439e1839c7e13e78a2d55f6c26c4601
blast <rodfval.fv@gmail.com>
2022-08-30 20:55:02 +0000

    asdda

--- build-nocache.sh
+++ build-nocache.sh
@@ -4,5 +4,7 @@ cd ./client
 bash ./build-nocache.sh
 cd ../FrittoPlayTelegramBot
 bash ./build-nocache.sh
+cd ../battuteAPI
+bash ./build-nocache.sh
 cd ../api
 bash ./build-nocache.sh
--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ cd ./client
 bash ./build.sh
 cd ../FrittoPlayTelegramBot
 bash ./build.sh
+cd ../battuteAPI
+bash ./build.sh
 cd ../api
 bash ./build.sh
