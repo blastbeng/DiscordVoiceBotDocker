4a0fd64e0415e657cbd8170e3a4631c24b05933b
blast <rodfval.fv@gmail.com>
2022-11-25 13:21:34 +0100

    ffff

--- build-restart.sh
+++ build-restart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo rm -rf /tmp/scemo-pezzente-api/fakeyou_voices.sqlite
 sudo systemctl stop docker-compose@scemo-pezzente.service
 docker-compose build
 sudo systemctl start docker-compose@scemo-pezzente.service
